171f123bab1724c711fccba2bc3c7acc844c40ab
Dilshan Ramesh <dilshan70@outlook.com>
2018-10-09 22:30:51 +0530

    Update insertionSort.py

---
+++ insertionSort.py
@@ -0,0 +1,21 @@
+
+
+
+a = []
+
+for x in range(5):
+    a.append(int(input("Enter Number : ")))
+print(a)
+
+
+def InsertionSort(A):
+    for j in range(1, len(A)):
+        key = A[j]
+        i = j - 1
+        while(i >=0 and A[i] > key):
+            A[i+1] = A[i]
+            i = i - 1
+        A[i+1] = key
+
+InsertionSort(a)
+print(a)
